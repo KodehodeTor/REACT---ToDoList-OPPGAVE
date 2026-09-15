c7df2043bef825979bddf9cb7ab50ac91de1fe3d
KodehodeTor <torjarand.26pg@gmail.com>
2026-09-15 14:01:52 +0200

    Added return statement for handleAddTask function.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,6 +24,35 @@ addTask(newTask)
 }
 
 return ( 
-    <>
-    </>
+   <div>
+    <h1>ToDoList</h1>
+    <form onSubmit={handleAddTask}>
+        {/* e.target referer til seg selv, så henter vi ut value og oppdater newTaskName til hva som står inni input field */}
+        <input type="text" onChange={(e) => setNewTaskName(e.target.value)} />
+        <button type="subtmit">Submit Task</button>
+        {/* Form for å kunne trykke enter og submit */}
+    </form>
+    {/* Sortering */}
+    <select name="" id=""
+    onChange={(e) => 
+        // Sort objekt, vi beholder previous, sorter by target value.
+        setSortOption((prev) => ({ ...prev, sortBy: e.target.value}))
+    }
+    >
+
+        {/* Filter */}
+        <option value="newest">Newest to Oldest</option>
+        <option value="oldest">Oldest to Newest</option>
+        <option value="a-to-z">A to Z</option>
+        <option value="z-to-a">Z to A</option>
+    </select>
+    {/* Hide or show completed  */}
+    <label htmlFor="hideorshow">
+        Hide completed tasks:
+        <input type="checkbox" id="hideorshow" checked={sortOption.hideCompleted}
+        onChange={(e) => ({
+            ...prev, hideCompleted: e.target.checked,
+        })} />
+    </label>
+   </div>
 )
