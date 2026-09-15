643a62fa6bd074857d44f916aa7d12929c551a74
KodehodeTor <torjarand.26pg@gmail.com>
2026-09-15 13:34:01 +0200

    Added note in ToDoItem to edit propdrilling to non-propdrilling after the basis is added.

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,5 +1,7 @@
 import ToDoItem from "./ToDoItem.jsx";
 
+//Når vi rendrer ToDoItem så snder vi den videre. (Dette er propdrilling og må endres på i etterkant Tor!)
+
 export default function ToDoList({
   data: { sortedData, deleteTask, editTask },
 }) {
