1d237a12e0c698ad15b1fffb0d86c6fd16c95538
KodehodeTor <torjarand.26pg@gmail.com>
2026-09-17 13:31:17 +0200

    Wrapped app with TodoProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import TodoProvider from "./context/TodoProvider.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <TodoProvider>
+      <App />
+    </TodoProvider>
   </StrictMode>,
 );
