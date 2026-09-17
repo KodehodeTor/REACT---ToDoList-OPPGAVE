4d2662559c0b79cf92a619a606e5910ab2544280
KodehodeTor <torjarand.26pg@gmail.com>
2026-09-17 14:41:11 +0200

    Removed sortedData useTodos from App()

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import ToDoList from "./components/ToDoList.jsx";
 import Header from "./components/Header.jsx";
-import { useTodos } from "./context/useTodos.js";
 
 function App() {
-  const { sortedData } = useTodos();
-
   return (
     <>
       <Header />
-      <ToDoList data={{ sortedData }} />
+      <ToDoList />
     </>
   );
 }
