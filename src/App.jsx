16cbc446cb1c1c0044c72f3c0f966366dbbe3145
KodehodeTor <torjarand.26pg@gmail.com>
2026-09-17 13:40:57 +0200

    moved todoData, setToDoData to TodoProvider

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,6 @@ function App() {
   // Vi lager en callback funksjon til useState så det blir initial state på useState. Dette er for LocalStorage.
 
   const [todoData, setToDoData] = useState(() => {
-    const savedData = localStorage.getItem("todoData");
-    // Finnes det noe i LocalStorage ? Hvis den er, lagre. Om ikke lager vi et tomt array.
-
-    // Når vi stringify toDoData så klarer den ikke å hente ut date objektet grunnet kompleksistet. Som løsning så redefiner vi timestamp som new Date.
-    return savedData
-      ? JSON.parse(savedData).map((task) => ({
-          ...task,
-          timestamp: new Date(task.timestamp),
-        }))
-      : [];
-  });
 
   // Noe av den samme logikken for sort for LocalStorage. Når en endring foregår så lagrer vi til LS.
   const [sortOption, setSortOption] = useState(() => {
--- src/context/TodoProvider.jsx
+++ src/context/TodoProvider.jsx
@@ -1,5 +1,21 @@
 import { TodoContext } from "./TodoContext";
+import { useState } from "react";
 
 export default function TodoProvider({ children }) {
-  return <TodoContext.Provider value={{}}>{children}</TodoContext.Provider>;
+  const [todoData, setToDoData] = useState(() => {
+    const savedData = localStorage.getItem("todoData");
+    // Finnes det noe i LocalStorage ? Hvis den er, lagre. Om ikke lager vi et tomt array.
+
+    // Når vi stringify toDoData så klarer den ikke å hente ut date objektet grunnet kompleksistet. Som løsning så redefiner vi timestamp som new Date.
+    return savedData
+      ? JSON.parse(savedData).map((task) => ({
+          ...task,
+          timestamp: new Date(task.timestamp),
+        }))
+      : [];
+  });
+
+  return (
+    <TodoContext.Provider value={{ todoData }}>{children}</TodoContext.Provider>
+  );
 }
