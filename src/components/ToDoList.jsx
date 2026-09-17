6ec311535220a49d02f16dd17d35483a048c746e
KodehodeTor <torjarand.26pg@gmail.com>
2026-09-17 14:40:02 +0200

    Added useTodos to ToDoList and removed object destructuring from the function parameter

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,8 +1,10 @@
 import ToDoItem from "./ToDoItem.jsx";
+import { useTodos } from "../context/useTodos.js";
 
 //Når vi rendrer ToDoItem så snder vi den videre. (Dette er propdrilling og må endres på i etterkant Tor!)
 
-export default function ToDoList({ data: { sortedData } }) {
+export default function ToDoList() {
+  const { sortedData } = useTodos();
   if (sortedData.length === 0) {
     return <h3>No tasks. Please add one to begin</h3>;
   }
